b8972b3ff9354b2619e7f1b3e77d92b8fac1d0b8
Ganesh <ganeshnathan06@gmail.com>
2023-08-28 00:51:17 +0530

    statistics heading

--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -8,25 +8,34 @@ import LocationList from "./LocationList";
 import MiniMaps from "./MiniMaps";
 
 import logo from "../Assets/logo.png";
-import statistics from "../Assets/sample_data.svg"
-import sampleData from './sampleData.json'
-
+import statistics from "../Assets/sample_data.svg";
+import sampleData from "./sampleData.json";
 
 function Dashboard() {
-   const [curData, setCurData] = useState(sampleData[0])
+	const [curData, setCurData] = useState(sampleData[0]);
 	const [currentLocation, setCurrentLocation] = useState(sampleData[0].start);
 	const [endLocation, setEndLocation] = useState(sampleData[0].end);
-   const [data, setData] = useState(sampleData)
+	const [data, setData] = useState(sampleData);
 
 	return (
 		<>
 			<div className="flex gap-4 p-4 min-h-[95vh]">
 				<div className="md:w-[30%] lg:w-[25%]">
 					<div className="h-[70%] overflow-auto">
-						<LocationList curData={curData}/>
+						<LocationList curData={curData} />
 					</div>
-					<div className="h-[30%] flex flex-col items-center justify-center gap-1 mt-2">
-                  <img src={statistics} alt="statistics" className="md:h-24 md:w-60 lg:h-32 lg:w-72"/>
+					<div className="h-[30%] flex flex-col items-center justify-center gap-4 mt-2">
+                  <div className="flex flex-col items-center justify-center">
+                  <Typography className="text-gray-400 md:text-md lg:text-lg">
+							Daily Statistics
+						</Typography>
+						<img
+							src={statistics}
+							alt="statistics"
+							className="md:h-12 md:w-60 lg:h-20 lg:w-72"
+						/>
+                  </div>
+                  <div className="flex flex-col items-center justify-center">
 						<Typography className="text-gray-400 md:text-md lg:text-lg">
 							Powered by
 						</Typography>
@@ -35,18 +44,16 @@ function Dashboard() {
 							alt="logo"
 							className="md:h-12 md:w-24 lg:h-16 lg:w-32"
 						></img>
+                  </div>
 					</div>
 				</div>
 				<div className="md:w-[70%] lg:w-[75%]">
 					<div className="h-[70%]">
-						<MainMap
-							curData={curData}
-                     mini={false}
-						/>
+						<MainMap curData={curData} mini={false} />
 					</div>
-               <div className="h-[30%]">
-                  <MiniMaps curData={curData} data={data} setCurData={setCurData}/>
-               </div>
+					<div className="h-[30%]">
+						<MiniMaps curData={curData} data={data} setCurData={setCurData} />
+					</div>
 				</div>
 			</div>
 		</>
